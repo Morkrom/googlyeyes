8f27d8a81fa97141d6d68e4a43fed78b4666ac16
Michael Mork <morkrom@protonmail.ch>
2016-09-04 17:08:07 -0400

    Initial commit

---
+++ Example/googlyeyes/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  googlyeyes
+//
+//  Created by Michael Mork on 09/04/2016.
+//  Copyright (c) 2016 Michael Mork. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+

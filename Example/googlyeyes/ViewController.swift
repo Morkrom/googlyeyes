8f6fd6ea282eb9e2dbd837c7806f7e117e0befa7
Michael Mork <morkrom@protonmail.ch>
2016-11-23 23:38:51 -0500

    bounds stay set on rotation

--- Example/googlyeyes/ViewController.swift
+++ Example/googlyeyes/ViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let leftEye = GooglyEye(frame: CGRect(x: 30, y: 300, width: 100, height: 100))
+    let leftEye = GooglyEye(frame: CGRect(x: 30, y: 300, width: 100, height: 150))
     let rightEye = GooglyEye(frame: CGRect(x: 200, y: 300, width: 100, height: 100))
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.purple
+        view.backgroundColor = UIColor(red: 73/255, green: 0, blue: 1.0, alpha: 1.0)
         view.addSubview(leftEye)
         view.addSubview(rightEye)
         

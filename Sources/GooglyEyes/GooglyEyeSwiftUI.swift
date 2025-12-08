aee5d30dd1e433269def7cce05100eaff099257d
Michael <3208281+Morkrom@users.noreply.github.com>
2025-12-07 17:20:06 -0800

    customizable percentage width in SwiftUI representable

--- Sources/GooglyEyes/GooglyEyeSwiftUI.swift
+++ Sources/GooglyEyes/GooglyEyeSwiftUI.swift
@@ -7,15 +7,22 @@ public struct GooglyEyeSwiftUI: UIViewRepresentable {
     private let size: CGSize
     
     @Binding private var percentilePupilMoved: CGPoint?
+    private var pupilDiameterPercentageWidth: CGFloat?
 
     public init(size: CGSize,
-                percentilePupilMoved: Binding<CGPoint?>) {
+                percentilePupilMoved: Binding<CGPoint?>,
+                pupilDiameterPercentageWidth: CGFloat?) {
+        self.pupilDiameterPercentageWidth = pupilDiameterPercentageWidth
         self.size = size
         self._percentilePupilMoved = percentilePupilMoved
     }
     
     public func makeUIView(context: Context) -> GooglyEye {
-        return GooglyEye(frame: .init(x: 0, y: 0, width: size.width, height: size.height))
+        let goog = GooglyEye(frame: .init(x: 0, y: 0, width: size.width, height: size.height))
+        if let customPup = pupilDiameterPercentageWidth {
+            goog.pupilDiameterPercentageWidth = customPup
+        }
+        return goog
     }
     
     public func updateUIView(_ uiView: GooglyEye, context: Context) {
